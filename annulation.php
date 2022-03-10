5d000ab7a5e6323d6b28c445cc352e351b783e85
eli5rose <eli5rose@yahoo.com>
2022-03-10 15:54:19 +0100

    updated Footer

---
+++ annulation.php
@@ -0,0 +1,40 @@
+<?php
+require_once "header.php";
+?>
+
+<div class="view">
+    <p>
+    <h1> Vérifiez les conditions d'annulation de votre séjour</h1>
+    <ul>
+        <h2>Avant de Réserver</h2>
+        Vous pouvez consulter les conditions d'annulation sur la page de l'annonce et pendant le processus de réservation, juste avant de payer.
+        <br>
+        <br>
+        <h2> Après avoir réservé</h2>
+        Vous pouvez consulter les conditions et les options d'annulation dans la section Voyages. Pour connaître les conditions d'annulation, il vous suffit de cliquer ou d'appuyer sur Afficher les détails du voyage. Les heures et dates stipulées dans les conditions d'annulation correspondent au fuseau horaire local du logement. Les délais d'annulation pour recevoir des remboursements sont calculés à partir de l'heure d'arrivée au logement dans le fuseau horaire local, ou 15 h si aucune heure d'arrivée n'est spécifiée.
+        Pour connaître le montant de votre remboursement, commencez par annuler votre réservation. Un décompte détaillé s'affichera alors. En fonction de la durée de votre séjour, de la date d'annulation et des conditions d'annulation applicables à votre réservation, vous pouvez obtenir un remboursement partiel si vous annulez après l'arrivée. En savoir plus sur les différentes conditions d'annulation.
+        N'oubliez pas que le montant remboursé ne sera jamais supérieur au montant payé au moment de l'annulation.
+        <br>
+        <br>
+        <h2>Cas de force majeure</h2>
+        Une urgence ou une catastrophe naturelle a perturbé votre réservation ? Vous avez peut-être droit à un remboursement pour cas de force majeure. Si vous devez annuler en raison du Covid-19, informez-vous sur les solutions qui s'offrent à vous.
+        <h2>Conditions d'annulation pour les hôtes</h2>
+        Si vous êtes un hôte ou si vous souhaitez en savoir plus sur les conditions d'annulation disponibles, veuillez consulter les conditions d'annulation de votre logement.
+        <h2>Problèmes pendant votre voyage</h2>
+        Si vous rencontrez des problèmes lorsque vous arrivez au logement et que l'hôte ne peut pas les résoudre rapidement, vous pouvez obtenir une protection en vertu de notre Politique de remboursement des voyageurs.
+        Si vous avez des questions, veuillez envoyer un e-mail à DonkeyStay.com ou appeler le (034) 4713 – 135
+        Merci d'avoir choisi DonkeyStay
+        Respectueusement votre,
+        DonkeyStay
+    </ul>
+    </p>
+</div>
+</body>
+
+</html>
+
+<div class="container-fluid border-top mt-5">
+
+    <?php
+    require_once "footer.php";
+    ?>
---
+++ terms_animaux.php
@@ -0,0 +1,142 @@
+<?php
+require_once "header.php";
+?>
+
+<div class="view">
+    <p>
+    <h1>OPTIONS ANIMAUX</h1>
+    <ul>
+        Notre communauté repose sur des principes d'inclusion, d'acceptation et de respect. Cela passe par l'accueil et le soutien des personnes en situation de handicap. En règle générale, les voyageurs qui ont besoin d'aménagements et de services adaptés, dans la mesure du raisonnable, ne doivent pas faire l'objet de discrimination ou se voir refuser un service lorsqu'ils utilisent DonkeyStay.
+        <br>
+        <br>
+        Tous les hébergements acceptant les animaux domestiques auront des restrictions, certaines plus que d'autres. Certaines des restrictions les plus courantes concernant les animaux domestiques incluent :
+        <br>
+        <br>
+        <ul>
+            <li>Chiens et chats uniquement.</li>
+            <li>Nombre maximum d'animaux par logement (généralement 1 ou 2).</li>
+            <li>Restrictions de taille, par ex. "chiens de moins de 50 livres".</li>
+            <li>Les animaux domestiques ne peuvent pas être laissés sans surveillance dans les logements.
+            <li>
+            <li>Les animaux domestiques ne sont autorisés que dans certaines logements acceptant les animaux domestiques.</li>
+            <li>Les animaux domestiques ne sont pas autorisés dans les espaces communs tels que les restaurants, les piscines, etc.</li>
+            <li>Les animaux domestiques doivent être tenus en laisse dans les espaces publics.</li>
+            <li>Et - cela devrait aller de soi ! - les clients sont responsables du nettoyage après leurs animaux de compagnie.</li>
+        </ul>
+        <br>
+        Localement, les hôtes peuvent être tenus de fournir plus ou moins d'aménagements raisonnables conformément aux exigences légales. Les hôtes et les voyageurs doivent se conformer à ces exigences légales.
+        Grâce à notre système de réservation en ligne, vous pourrez vérifier la disponibilité des chambres (pour la plupart des hôtels) et effectuer votre réservation. Pour votre tranquillité d'esprit, vous recevrez par courriel une confirmation de réservation, incluant les détails de votre réservation. Il est recommandé de conserver une copie de cette confirmation de réservation, de préférence en imprimant la page.
+        <br>
+        <br>
+        <h2> Animaux d'assistance et animaux de soutien émotionnel</h2>
+        <strong>Animal d'assistance :</strong>un chien ou un cheval miniature dressé de manière spécifique afin d'effectuer un travail ou des tâches pour aider des personnes en situation de handicap.
+        <br>
+        <br>
+        <li>Ce que nous permettons :
+            <ul>
+                <li>Les voyageurs sont autorisés à être accompagnés d'animaux d'assistance pendant un séjour ou une expérience et ne sont pas tenus de divulguer la présence d'un animal d'assistance avant de réserver. Un hôte peut bénéficier d'une dérogation dans certaines circonstances (par exemple, si l'animal d'assistance menace directement sa santé ou sa sécurité).</li>
+                <li>Les hôtes sont uniquement autorisés à poser les questions suivantes sur les raisons pour lesquelles un voyageur a besoin d'un animal d'assistance :
+                    <ul>
+                        <li>Le voyageur a-t-il besoin de son animal d'assistance en raison d'un handicap ?</li>
+                        <li>Pour quel travail ou quelle tâche l'animal d'assistance a-t-il été dressé ?</li>
+                    </ul>
+            </ul>
+            <br>
+        </li>
+        <li>Ce que nous ne permettons pas :
+            <ul>
+                <ul>Lorsqu'un voyageur est accompagné d'un animal d'assistance, les hôtes ne sont pas autorisés à : </ul>
+                <li>Refuser une réservation</li>
+                <li>Appliquer des frais pour les animaux ou d'autres frais supplémentaires</li>
+                <li>Appliquer un traitement différencié</li>
+                <li>Utiliser un langage discriminatoire</li>
+                <li>Imposer des règles différentes aux voyageurs</li>
+                <ul>L'animal d'assistance d'un voyageur ne doit pas :</ul>
+                <li>Être hors de contrôle</li>
+                <li>Ne pas être propre</li>
+                <li>Être laissé seul dans le logement sans autorisation</li>
+                <li>Pénétrer dans les espaces dont l'accès n'est pas autorisé au voyageur</li>
+                <li>Se trouver dans un espace public sans harnais, laisse ou système de retenue approprié, et sans être surveillé par le voyageur</li>
+                <br>
+            </ul>
+            <strong>Animal de soutien émotionnel :</strong>un animal qui tient compagnie, soulage de la solitude ou aide à lutter contre la dépression, l'anxiété ou certaines phobies, mais qui n'a pas nécessairement fait l'objet d'un dressage particulier pour effectuer certaines tâches afin d'aider les personnes en situation de handicap.
+            <br>
+        <li>Ce que nous permettons:</li>
+        <ul>
+            <li>À moins que le séjour n'ait lieu dans l'État de New York ou en Californie (États-Unis) ou dans tout autre endroit où la loi applicable l'interdit :
+                <ul>
+                    <li>Les hôtes peuvent facturer des frais pour les animaux aux voyageurs accompagnés d'un animal de soutien émotionnel.</li>
+                    <li>Les hôtes sont autorisés à refuser la présence d'animaux de soutien émotionnel lors d'un séjour ou d'une expérience.</li>
+                </ul>
+            </li>
+        </ul>
+        <li>Ce que nous ne permettons pas :
+            <ul>
+                <li>Là où les hôtes sont tenus d'accepter les animaux de soutien émotionnel (sauf si l'hôte bénéficie d'une dérogation), un hôte ne peut pas :
+                    <ul>
+                        <li>Facturer des frais pour les animaux, refuser un voyageur, ou appliquer des règles différentes, un traitement particulier ou se comporter différemment à l'égard d'un voyageur accompagné d'un animal de soutien émotionnel
+                        </li>
+                        <li>Demander des informations ou des documents concernant l'animal de soutien émotionnel d'un voyageur au-delà des questions indiquées ci-dessus pour les animaux d'assistance</li>
+                    </ul>
+                </li>
+                <li>Là où les hôtes sont tenus d'accepter les animaux de soutien émotionnel, l'animal de soutien émotionnel d'un voyageur ne doit pas :
+                    <ul>
+                        <li>Être hors de contrôle</li>
+                        <li>Ne pas être propre</li>
+                        <li>Être laissé seul dans le logement sans autorisation</li>
+                        <li>Pénétrer dans les espaces dont l'accès n'est pas autorisé au voyageur</li>
+                        <li>Se trouver dans un espace public sans harnais, laisse ou système de retenue approprié, et sans être surveillé par le voyageur</li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <br>
+        <h2>Aménagements raisonnables</h2>
+        <br>
+        Nous encourageons les hôtes et les voyageurs à discuter des aménagements raisonnables avant toute réservation. Les hôtes doivent s'efforcer de répondre aux demandes raisonnables des voyageurs qui présentent des besoins en matière d'accessibilité.
+        <li>Ce que nous permettons :
+            <ul>
+                <li>Les voyageurs sont autorisés à demander un aménagement raisonnable qui leur permettrait d'avoir accès à un séjour ou une expérience, de communiquer à l'occasion d'une réservation ou de participer à une expérience. Les hôtes sont autorisés à faire une contre-proposition raisonnable à la demande initiale d'aménagement raisonnable des voyageurs.
+                </li>
+                <li>Les hôtes sont autorisés à refuser certaines demandes déraisonnables ou irréalisables qui :
+                    <ul>
+                        <li>Font courir un risque de sécurité plus élevé à l'hôte ou à d'autres personnes</li>
+                        <li>Modifient fondamentalement la nature d'une expérience ou ont un impact sur la façon dont les autres voyageurs peuvent en profiter</li>
+                        <li>Nécessitent une modification structurelle d'un immeuble ou d'un logement
+                        </li>
+                        <li>Exigent d'assumer des responsabilités supplémentaires, prennent beaucoup de temps, ou représentent une charge physique ou financière importante pour l'hôte</li>
+                        <li>Obligent un hôte à enfreindre la législation locale ou les règlements de la copropriété/de l'immeuble</li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li>Ce que nous ne permettons pas :
+            <ul>
+                <li>Les hôtes ne sont pas autorisés à refuser la demande raisonnable d'aménagement d'un voyageur, lorsque la demande est spécifique, clairement exprimée, effectuée avec un préavis suffisant et n'est pas déraisonnable ou irréalisable (voir la liste des demandes qui sont considérées comme déraisonnables ou irréalisables ci-dessus).</li>
+                <li>Les hôtes ne sont pas autorisés à prendre l'engagement de fournir un aménagement raisonnable et à ne pas le respecter au moment de la réservation.</li>
+            </ul>
+        <li>Considérations supplémentaires :
+            <ul>
+                <li>Un hôte ne sera pas pénalisé si son manquement à réaliser un aménagement raisonnable est considéré comme étant indépendant de sa volonté ou si l'hôte a objectivement démontré pourquoi la demande est déraisonnable ou irréalisable pour lui.
+                </li>
+            </ul>
+        </li>
+        </li>
+        <br>
+        <h2>Quelles sont les conséquences du non-respect de nos politiques par un hôte ou un voyageur ?</h2>
+        Nous demandons à notre communauté de travailler ensemble pour rendre DonkeyStay aussi accessible que possible. DonkeyStay pourra prendre des mesures pouvant aller jusqu'à la suspension ou le retrait de l'hôte ou du voyageur de la plate-forme DonkeyStay s'il refuse de se conformer à nos politiques.
+        Si vous avez des questions, veuillez envoyer un e-mail à DonkeyStay.com ou appeler le (034) 4713 – 135
+        Merci d'avoir choisi DonkeyStay
+        Respectueusement votre,
+        DonkeyStay
+    </ul>
+    </p>
+</div>
+</body>
+</html>
+
+<div class="container-fluid border-top mt-5">
+
+    <?php
+    require_once "footer.php";
+    ?>
