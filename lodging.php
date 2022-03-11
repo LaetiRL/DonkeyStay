fbff66062bae36dabd2e144a6340e46702be3bd8
Laetitia Rodriguez <rodriguezl.laetitia@gmail.com>
2022-03-11 17:26:23 +0100

    correction du foreach sur les images

--- lodging.php
+++ lodging.php
@@ -3,7 +3,7 @@ $titleWeb = "Mes logements";
 require "header.php";
 $user_id = $_SESSION['user_id'];
 
-$lodgingQuery = $dbh->prepare("SELECT room.*,img FROM room INNER JOIN image ON image.room_id = room.id WHERE room.user_id =:userId");
+$lodgingQuery = $dbh->prepare("SELECT * FROM room WHERE room.user_id =:userId");
 
 $lodgingQuery->bindValue(":userId", $user_id, PDO::PARAM_INT);
 $lodgingQuery->execute();
@@ -18,19 +18,28 @@ $lodgings = $lodgingQuery->fetchAll(PDO::FETCH_ASSOC);
     <?php
     foreach ($lodgings as $lodging) {
         echo '<div style="display: flex;">';
-        echo '<div>';
-        echo '<img src="' . $lodging['img'] . '" width="300px" height="auto" alt="">';
-        echo '</div>';
-        echo '<div>';
-        echo '<div><span>' . $lodging['hometype_id'] . ': ' . $lodging['roomtype_id'] . ' - ' . $lodging['city'] . '</span></div>';
-        echo '<div><h2>' . $lodging['title'] . '</h2></div>';
-        echo '<div><span>' . $lodging['capacity_id'] . ' - ' . $lodging['nb_bedroom'] . ' - ' . $lodging['nb_bathroom'] . '</span></div>';
-        echo '<div><span>' . $lodging['has_wifi'] . ' - ' . $lodging['has_kitchen'] . '</span></div>';
-        echo '<span><button type="button" class="btn btn-secondary"><a href="modifyLodging.php?id=' . $lodging['id'] . '">Modifier</a></button><button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            echo '<div>';
+                $lastestAddImgQuery = $dbh->query('SELECT * FROM image WHERE room_id ='.$lodging['id'].' LIMIT 1');
+                $lastestAddImg = $lastestAddImgQuery->fetchall(PDO::FETCH_ASSOC);
+
+                foreach ($lastestAddImg as $row_lastestAddImg) {
+                    if ($row_lastestAddImg['room_id'] === $lodging['id'] ) {
+                        
+                        echo '<img src="'.$row_lastestAddImg['img'].'" alt="" class="card-img-top">';
+                    } 
+                }
+                
+            echo '</div>';
+            echo '<div>';
+                echo '<div><span>' . $lodging['hometype_id'] . ': ' . $lodging['roomtype_id'] . ' - ' . $lodging['city'] . '</span></div>';
+                echo '<div><h2>' . $lodging['title'] . '</h2></div>';
+                echo '<div><span>' . $lodging['capacity_id'] . ' - ' . $lodging['nb_bedroom'] . ' - ' . $lodging['nb_bathroom'] . '</span></div>';
+                echo '<div><span>' . $lodging['has_wifi'] . ' - ' . $lodging['has_kitchen'] . '</span></div>';
+                echo '<span><button type="button" class="btn btn-secondary"><a href="modifyLodging.php?id=' . $lodging['id'] . '">Modifier</a></button><button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                     Supprimer
                   </button></span>';
-        echo '<span>' . 'Prix/nuit: ' . $lodging['price'] . '€' . '</span>';
-        echo '</div>';
+                echo '<span>' . 'Prix/nuit: ' . $lodging['price'] . '€' . '</span>';
+            echo '</div>';
         echo '</div>';
 
         echo '<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
