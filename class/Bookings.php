18d4bc2a4dadeaca56b4377834f367fa0246118f
Jérémy Chalumeau <jchalumeau77@gmail.com>
2022-03-16 15:43:39 +0100

    pour la relou

---
+++ class/Bookings.php
@@ -0,0 +1,14 @@
+<?php
+
+class Bookings {
+
+    
+    public function getBookingsBetween (\DateTime $start, \DateTime $end): array
+    {
+        $dbh = new PDO('mysql:dbname=DonkeyStay;host=127.0.0.1', 'root', '');
+        $sql = "SELECT * FROM booking WHERE start_date BETWEEN '{$start->format('Y-m-d')}' AND '{$end->format('Y-m-d')}'";
+        $stm = $dbh->query($sql);
+        $results = $stm->fetchAll(PDO::FETCH_ASSOC);
+        return $results;
+    }
+}
